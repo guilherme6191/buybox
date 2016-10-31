678a6594b622f8727fbbb43b59ffec067f31d95c
Guilherme6191 <gui.vasconcelos.21@gmail.com>
2016-10-31 12:12:21 -0200

    Profile update done.

--- app/components/Messages.js
+++ app/components/Messages.js
@@ -25,7 +25,7 @@ class Messages extends React.Component {
             <div role="alert" className={ messageStyle } className="alert alert-success">
                 <a href="#"
                    onClick={this.onClose.bind(this)}
-                   class="close">&times;</a>
+                   className="close">&times;</a>
 
                 {this.props.messages.success.map((message, index) => <div key={index}>{message.msg}</div>)}
             </div>
@@ -33,7 +33,7 @@ class Messages extends React.Component {
             <div role="alert" className={ messageStyle } className="alert alert-danger">
                 <a href="#"
                    onClick={this.onClose.bind(this)}
-                   class="close">&times;</a>
+                   className="close">&times;</a>
 
                 {this.props.messages.error.map((message, index) => <div key={index}>{message.msg}</div>)}
             </div>
@@ -41,7 +41,7 @@ class Messages extends React.Component {
             <div role="alert" className={ messageStyle } className="alert alert-info">
                 <a href="#"
                    onClick={this.onClose.bind(this)}
-                   class="close">&times;</a>
+                   className="close">&times;</a>
 
                 {this.props.messages.info.map((message, index) => <div key={index}>{message.msg}</div>)}
             </div>
