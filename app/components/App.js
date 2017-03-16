552b4911d100680d5ba0f77991d938351476e8ab
Guilherme6191 <gui.vasconcelos.21@gmail.com>
2017-03-16 12:48:53 -0300

    Started notification via browser.

--- app/components/App.js
+++ app/components/App.js
@@ -3,15 +3,30 @@ import Header from './Header';
 import Footer from './Footer';
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header/>
-        {this.props.children}
-        <Footer/>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        if (!("Notification" in window)) {
+            console.log("[NOTE]:This browser does not support desktop notification.");
+        }
+        else if (Notification.permission !== "denied") {
+            Notification.requestPermission(function (permission) {
+                console.log("[NOTE]:User granted permission for desktop notification.")
+            });
+        } else if (Notification.permission !== "denied") {
+            console.log("[NOTE]: User has already granted permission for desktop notification.")
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                {this.props.children}
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 export default App;
